a6da22d8bfe696a87a3391c0c3d8a63f2a03b29c
Chetan Bhoi <bhoichetan23@gmail.com>
2025-02-04 00:50:33 +0530

    login and navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,53 +1,58 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 const Login = () => {
-  const [email, SetEmailId] = useState("chetan@gmail.com");
-  const [password, SetPassword] = useState("Chetan@123");
 
-  const handleLogin = async () => {
-    try {
-      const res = axios.post("http://localhost:5000", {
-        email,
-        password,
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const [emailId, setEmailId] = useState("chetan@gmail.com");
+    const [password, setPassword] = useState("Chetan@123");
   
-  return (
-    <div className="flex justify-center my-10">
-      <div className="card bg-base-300 w-96 shadow-xl">
-        <div className="card-body">
-          <div>
-            <label className="form-control w-full max-w-xs my-2">
-              <div className="label">
-                <span className="label-text">Email ID:</span>
-              </div>
-              <input
-                type="text"
-                value={emailId}
-                className="input input-bordered w-full max-w-xs"
-                onChange={(e) => setEmailId(e.target.value)}
-              />
-            </label>
-            <label className="form-control w-full max-w-xs my-2">
-              <div className="label">
-                <span className="label-text">Password</span>
-              </div>
-              <input
-                type="password"
-                value={password}
-                className="input input-bordered w-full max-w-xs"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
+    // const handleLogin = async () => {
+    //   try {
+    //     const res = axios.post(BASE_URL + "/login", {
+    //       email,
+    //       password,
+    //     }, {withCredentials: true});
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
+
+    return (
+      <div className="flex justify-center my-10">
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <div className="card-body ">
+            <h2 className="card-title text-center ">Login Now</h2>
+            <div>
+              <label className="form-control w-full max-w-xs my-2">
+                <div className="label">
+                  <span className="label-text">Email ID:</span>
+                </div>
+                <input
+                  type="text"
+                  value={emailId}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setEmailId(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full max-w-xs my-2">
+                <div className="label">
+                  <span className="label-text">Password</span>
+                </div>
+                <input
+                  type="password"
+                  value={password}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="card-actions justify-center">
+              <button className="btn btn-primary">Login</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  
+  
+    };
+ 
 export default Login;
