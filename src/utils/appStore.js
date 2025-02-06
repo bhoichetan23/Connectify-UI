5c7c8ed9409bf3890a854d1084ff10250102d27f
Chetan Bhoi <bhoichetan23@gmail.com>
2025-02-06 23:08:13 +0530

    signup

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,7 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 
 const appStore = configureStore({
-    reducer:{},
+    reducer:{
+        user: userReducer,
+    },
 
 
 });
