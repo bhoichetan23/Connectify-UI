600c102174f1c5022c3c67468fbbc88072a12955
Chetan Bhoi <bhoichetan23@gmail.com>
2025-02-11 00:19:23 +0530

    profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const user = useSelector((store) =>{store.user});
+  if(user)
+  return <div>
+    <EditProfile user={user} />
+  </div>;
 };
 
 export default Profile;
